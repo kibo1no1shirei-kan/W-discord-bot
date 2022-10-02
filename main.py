7f07af716dcb8239f7a708c209a19939bbb1ce73
kibo1no1shirei-kan <96419861+kibo1no1shirei-kan@users.noreply.github.com>
2022-10-02 12:23:04 +0000

    changes

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import nextcord
+from nextcord.ext import commands
+import aiohttp
+import bs4
 
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+
+
+
+# this is the commnand to search for related things in arknighs
+
+@bot.command()
+async def arknights(ctx,arknights):
+    
+    
