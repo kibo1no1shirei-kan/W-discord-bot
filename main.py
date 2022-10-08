9cbdcf905cfc2813967f7bf2b1a78e8860f29aaa
kibo1no1shirei-kan <96419861+kibo1no1shirei-kan@users.noreply.github.com>
2022-10-08 08:17:41 +0000

    first command test is now published

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from nextcord.ext import commands
 import aiohttp
 import bs4
 
+
+
 intents = nextcord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", description=description, intents=intents)
+
+bot = commands.Bot(command_prefix="w", description=None, intents=intents)
+
+@bot.command()
+async def test(ctx,message):
+    await ctx.send(message)
 
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
-
-
-
 # this is the commnand to search for related things in arknighs
 
 @bot.command()
 async def arknights(ctx,arknights):
-    pass
+    arknights = "null"
+    return
     
+bot.run("MTAxODAzNjA3MzI1Njk5Njg3Ng.G8Kf5k.irE21heU8pEnoCvCmArqwvounQoDZdjfTpVFfM")
+
