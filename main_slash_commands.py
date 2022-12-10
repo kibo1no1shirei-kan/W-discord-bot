b3f01cfdbf59665921609051903dbb257a9ae4d8
kibo1no1shirei-kan <96419861+kibo1no1shirei-kan@users.noreply.github.com>
2022-12-10 12:35:27 +0800

    added arknights command verison boit version 2.0

--- main_slash_commands.py
+++ main_slash_commands.py
@@ -1,47 +1,59 @@
 import nextcord
 import re
+import asyncio
 from nextcord.ext import commands, tasks
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium import Chrome
-
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import bs4
+from bs4 import BeautifulSoup
 
 
 intents = nextcord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-with open('token.txt', 'r') as f:
-    contents = f.read()
-
 bot = commands.Bot(command_prefix="w", description=None, intents=intents)
 
 @bot.slash_command()
-async def test(ctx,message):
-    await ctx.send(message)
+async def test(interaction,message):
+    await interaction.send(message)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
+@bot.event
+async def on_command_error(interaction,error):
+    await interaction.send(error)
 
 @bot.event
-async def on_command_error(ctx,error):
-    await ctx.send(error)
-
-@bot.event
-async def repeat(ctx,word):
-    await ctx.send(word)
-
-
+async def repeat(interaction,word):
+    await interaction.send(word)
 
 @bot.slash_command()
-async def arknights(ctx,operator):
-    pass
+async def arknights(interaction,operator):
+    await interaction.response.defer()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=options)
+    options.page_load_strategy = 'normal'
+    driver.get(f"https://aceship.github.io/AN-EN-Tags/akhrchars.html?opname={operator}&story=0")
+    await asyncio.sleep(15)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    operator_quotes = soup.find_all("td")
+    gender = driver.find_element(By.ID, "op-gender").text
+    chara_image_elements = driver.find_element(By.CLASS_NAME, "chara-image").get_attribute("src")
+    operator_quotest = operator_quotes[1].text
+    author = interaction.user
+    authoravatar = author.display_avatar.url
+    embed=nextcord.Embed(title=operator, url=f"https://aceship.github.io/AN-EN-Tags/akhrchars.html?opname={operator}", description=operator_quotest , color=0x0923e1)
+    embed.set_author(name=author, icon_url=authoravatar)
+    embed.add_field(name="Sex", value=gender, inline=True) #basic file
+    #embed.add_field(name="Combat Experience", value=exp, incline=False)
 
-
-
-
-
-
-bot.run(contents)
+    embed.set_author(name=author, icon_url=authoravatar) 
+    embed.set_image(url=chara_image_elements)
+    await interaction.followup.send(embed=embed)
+    driver.quit()
