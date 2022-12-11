7c372d53b1659ef769b113280f12e66002cc9031
kibo1no1shirei-kan <96419861+kibo1no1shirei-kan@users.noreply.github.com>
2022-12-11 12:51:54 +0800

    Update main_slash_commands.py

--- main_slash_commands.py
+++ main_slash_commands.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 
 
 intents = nextcord.Intents.default()
+
 intents.members = True
 intents.message_content = True
 
@@ -42,17 +43,25 @@ async def arknights(interaction,operator):
     driver.get(f"https://aceship.github.io/AN-EN-Tags/akhrchars.html?opname={operator}&story=0")
     await asyncio.sleep(15)
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    operator_quotes = soup.find_all("td")
+    td = soup.find_all("td")
     gender = driver.find_element(By.ID, "op-gender").text
     chara_image_elements = driver.find_element(By.CLASS_NAME, "chara-image").get_attribute("src")
-    operator_quotest = operator_quotes[1].text
+    operator_quotes = td[1].text
+    combatexp = td[9].text
+    placeofbirth = td[11].text
+    dateofbirth = td[13].text
+    race = td[15].text
+    height = td[17].text
     author = interaction.user
     authoravatar = author.display_avatar.url
-    embed=nextcord.Embed(title=operator, url=f"https://aceship.github.io/AN-EN-Tags/akhrchars.html?opname={operator}", description=operator_quotest , color=0x0923e1)
+    embed=nextcord.Embed(title=operator, url=f"https://aceship.github.io/AN-EN-Tags/akhrchars.html?opname={operator}", description=operator_quotes , color=0x0923e1)
     embed.set_author(name=author, icon_url=authoravatar)
-    embed.add_field(name="Sex", value=gender, inline=True) #basic file
-    #embed.add_field(name="Combat Experience", value=exp, incline=False)
-
+    embed.add_field(name="Sex", value=gender, inline=False) #basic file
+    embed.add_field(name="Combat Experience", value=combatexp, inline=False)
+    embed.add_field(name="Place of Birth", value=placeofbirth, inline=False)
+    embed.add_field(name="Date of Birth", value=dateofbirth, inline=False)
+    embed.add_field(name="Race", value=race, inline=False)
+    embed.add_field(name="Height", value=height, inline=False)
     embed.set_author(name=author, icon_url=authoravatar) 
     embed.set_image(url=chara_image_elements)
     await interaction.followup.send(embed=embed)
